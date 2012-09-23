b4a4e4aba5d0393413f7f3e928a63943e05aa8c6
Thomas Schutzmeier <tschutzmeier@gmail.com>
2012-09-23 08:37:05 -0400

    Buffer: simplify data types

--- src/Buffer.hs
+++ src/Buffer.hs
@@ -1,87 +1,89 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Buffer where
 
 import Cursor
 import Data.Maybe
 import Control.Monad.State
+import Control.Applicative
+import System.IO
 
 type Point = (Int, Int)
 data Move = MLeft | MRight | MUp | MDown
-data Rect = Rect Point Int Int 
+data Rect = Rect { rectOrigin :: Point
+                 , rectWidth ::  Int
+                 , rectHeight :: Int }
 
--- extend with tags, like truncated lines etc. 
-data BufferView = BufferView { viewRect :: Rect
-                             , viewVisible :: [String] }
 instance Show Rect where
     show (Rect pt w h) = "Rect in " ++ show pt ++ ", width: " ++ show w ++ ", height: " ++ show h
                 
-instance Show BufferView where
-    show (BufferView rect@(Rect pt w h) str) =
-                (show rect ) ++ "\n"
-             ++ "Content:\n"
-             ++ (replicate w '-') ++ "\n"
-             ++ (unlines $ str)
-             ++ (replicate (h - length str) '\n')
-             ++ (replicate w '-') ++ "\n"
-
 data Buffer = Buffer { bufferCursor :: TextCursor
                      , bufferTitle :: String
-                     , bufferView :: BufferView
+                     , viewRect :: Rect
                      } deriving Show
 
 type BufferST m = StateT Buffer m
 
+type BufferView = [String]
+
+emptyBuffer :: Buffer
+emptyBuffer = buffer [[]] "" (0,0) 0 0
+
+loadFile :: FilePath -> IO Buffer
+loadFile path = do handle <- openFile path ReadMode 
+                   contents <- hGetContents handle
+                   let crs = textCursor (lines contents) (0,0)
+                   return $ (\buf -> buf { bufferCursor = crs
+                                         , bufferTitle = path }) emptyBuffer
+                   
+bufCursorPos :: Buffer -> Point
+bufCursorPos = cursorPosition . bufferCursor
+
+bufRelCursorPos :: Buffer -> (Int, Int)
+bufRelCursorPos buf = (x - x0, y - y0)
+    where (x,y) = cursorPosition $ bufferCursor buf
+          (x0, y0) = rectOrigin $ viewRect buf
+
 slice :: Int -> Int -> [a] -> [a]
 slice start end = take (end - start) . drop start
 
-createView :: TextCursor -> Rect -> BufferView
-createView crs rect@(Rect (x,y) w h) = BufferView rect lineSlice
-    where str  = fromCursor crs
-          textSlice = slice y (y+h) str
-          lineSlice = map (slice x (x+w)) textSlice
+visibleC :: Buffer -> BufferView
+visibleC buf = map (slice x (x+w)) $ slice y (y+h) $ fromCursor $ bufferCursor buf
+    where (Rect (x,y) w h) = viewRect buf
  
 buffer :: [String] -> String -> Point -> Int -> Int -> Buffer
-buffer str title pt w h = Buffer crs title $ createView crs $ Rect pt w h
-    where crs = textCursor str pt
- 
-bufferCursorPosition :: Buffer -> Point
-bufferCursorPosition = cursorPosition . bufferCursor
+buffer str title pt w h = Buffer { bufferCursor = textCursor str pt
+                                 , bufferTitle  = title
+                                 , viewRect     = Rect pt w h
+                                 }
  
 modifyCursor :: (Monad m) => (TextCursor -> Maybe TextCursor) -> BufferST m Bool
-modifyCursor f = do
-   st <- get
-   case f $ bufferCursor st of
-      Nothing -> return False
-      Just newcrs -> do let cpos = cursorPosition newcrs
-                        let rect = viewRect $ bufferView st
-                        if not $ pointInRect cpos rect
-                           then do let newrect = getRect rect cpos
-                                   let newView = createView newcrs newrect
-                                   put (st { bufferCursor = newcrs, bufferView = newView } )
-                           else put (st { bufferCursor = newcrs, bufferView = createView newcrs rect} )
-                        return True
- 
- 
-resizeBuffer :: (Monad m) => Int -> Int -> BufferST m Bool
-resizeBuffer w h = do
-   st <- get
-   let (Rect pt _ _) = viewRect $ bufferView st
-   let crs = bufferCursor st
-   put $ st { bufferView = createView crs $ Rect pt w h }
-   modifyCursor $ textMoveToM $ cursorPosition crs
- 
+modifyCursor f = do get >>= \x -> maybe (return False) (\x -> setCursor x >> return True) (f $ bufferCursor x)
+    where setCursor crs = do modify (\x -> x { bufferCursor = crs })
+                             updateViewRect
+
+updateViewRect :: (Monad m) => BufferST m ()
+updateViewRect = modify (\x -> x { viewRect = updateRect (bufCursorPos x) (viewRect x) })
+
+resizeBuffer :: (Monad m) => Int -> Int -> BufferST m ()
+resizeBuffer w h = do modify (\x -> x { viewRect = resizeRect (viewRect x) w h })
+                      updateViewRect
+
 inInterval :: Int -> (Int, Int) -> Bool
 inInterval z (x,y) = z >= x && z < y
               
 pointInRect :: Point -> Rect -> Bool
 pointInRect (x,y) (Rect (rx, ry) w h) = inInterval x (rx, rx + w)  && inInterval y (ry, ry + h)
  
-getRect :: Rect -> Point -> Rect
-getRect (Rect (rx,ry) w h) (x,y) | x >= w + rx = Rect (x - w + 1, ry) w h
+getRect :: Point -> Rect -> Rect
+getRect (x,y) (Rect (rx,ry) w h) | x >= w + rx = Rect (x - w + 1, ry) w h
                                  | x < rx      = Rect (x, ry) w h
                                  | y >= h + ry = Rect (rx,  y - h + 1) w h
                                  | y < ry      = Rect (rx, y) w h
                                  | otherwise   = Rect (rx,ry) w h
                                          
+updateRect :: Point -> Rect -> Rect
+updateRect pt rect = if (not $ pointInRect pt rect) then getRect pt rect else rect
+                                                 
 resizeRect :: Rect -> Int -> Int -> Rect
 resizeRect (Rect p _ _) w h = Rect p w h
 
