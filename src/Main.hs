c7d0d3601e61cbc11085a15c4db46ac1baaf60ca
Thomas Schutzmeier <tschutzmeier@gmail.com>
2012-09-23 08:37:54 -0400

    Main: some refinement and experiments

--- src/Main.hs
+++ src/Main.hs
@@ -11,29 +11,37 @@ main = do
   vty <- mkVty
   reserve_display (terminal vty)
   show_cursor (terminal vty)
-  bounds <- display_bounds (terminal vty)
-  let buf = buffer ["Welcome!","", "This is an amazing editor"] "Welcome" (0,0) (fromIntegral $ region_width bounds) (fromIntegral $ region_height bounds)
   show_cursor (terminal vty)
+  buf <- loadFile "/home/tom/dev/ziped/src/Buffer.hs"
   runStateT (loop vty) buf
             
-loop vty = do buf <- get
-              lift $ update vty $ pic_for_image (display $ viewVisible $ bufferView buf) 
+loop vty = do bounds <- lift $ display_bounds (terminal vty)
+              resizeBuffer (fromIntegral $ region_width bounds) ((fromIntegral $ region_height bounds) - 1)
+              buf <- get
+              lift $ update vty $ pic_for_image $ display  buf $ fromIntegral $ region_height bounds
               -- setCursor must come after update, otherwise cursor will be hidden!
-              setCursor vty
+              setCursorPos vty
               k <- lift $ next_event vty
-              case k of
-                  EvResize w h  -> resizeBuffer w h
-                  EvKey KEsc [] -> lift $ shutdown vty >> return True
-                  k@(EvKey c m) -> performAction k
-                  _             -> return False
+              handleKey k vty
               loop vty
 
-display buf = foldr1 (<->) $ map (\x -> string current_attr (f x)) buf
-    where f [] = " "
+handleKey (EvResize w h)  vty = resizeBuffer w h >> return True
+handleKey (EvKey KEsc []) vty = lift $ shutdown vty >> return True
+handleKey (k@(EvKey c m)) vty = performAction k
+handleKey _               vty = return False
+
+display buf windowLines = text <-> emptyLines <-> status
+    where text = foldr1 (<->) $ map (\x -> string current_attr (f x)) visLines
+          visLines = visibleC buf
+          emptyLines = foldr (<->) empty_image $ map (string current_attr . f) $ replicate nEmptyLines " "
+          nEmptyLines = if (length visLines < windowLines) then (windowLines - (length visLines) - 1) else 0
+          cpos = cursorPosition $ bufferCursor buf
+          status = string (with_back_color current_attr blue) ("   cursor: " ++ show cpos ++ "  " ++ bufferTitle buf)
+          f [] = " "
           f x = x
 
-setCursor vty  = do b <- get
-                    let (x,y) = cursorPosition $ bufferCursor b
-                    lift $ do show_cursor (terminal vty)
-                              set_cursor_pos (terminal vty) (fromIntegral x) (fromIntegral y)
+setCursorPos vty  = do b <- get
+                       lift $ do show_cursor (terminal vty)
+                                 let (dx, dy) = bufRelCursorPos b
+                                 set_cursor_pos (terminal vty) (fromIntegral dx) (fromIntegral dy)
                                 
