1b220af39e45dbba0765cd33a1e2856969b7e4d5
Thomas Schutzmeier <tschutzmeier@gmail.com>
2012-09-24 07:53:20 -0400

    Add actions. Add repeated movements

--- src/Editor.hs
+++ src/Editor.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
 module Editor where
 
 import Cursor
 import Buffer
 import Graphics.Vty
+import Data.Maybe
+import Control.Monad
 
 import qualified Data.Map as M
 
@@ -18,10 +21,16 @@ data Action = MoveLeft
             | DeleteBack
             | DeleteLine
             | DeleteLineBack
+            | MoveEnd
+            | MoveBegin
+            | MoveEndLine
+            | MoveBeginLine
+            | KillToEnd
 
-keyAction :: Key -> Action
-keyAction = undefined
+repeatMaybe :: (a -> Maybe a) -> a -> Maybe a
+repeatMaybe f x = maybe (return x) (repeatMaybe f) (f x)
 
+-- TODO: here we need information on display size for PageDown and the like!!
 actionMap :: Action ->  (TextCursor -> Maybe TextCursor)
 actionMap MoveLeft       = fmapM (move Bwd)
 actionMap MoveRight      = fmapM (move Fwd)
@@ -34,6 +43,11 @@ actionMap Delete         = fmapM (delete Fwd)
 actionMap DeleteBack     = fmapM (delete Bwd)
 actionMap DeleteLine     = delete Fwd
 actionMap DeleteLineBack = delete Bwd
+actionMap MoveEnd        = Just . moveEnd Fwd
+actionMap MoveBegin      = Just . moveEnd Bwd
+actionMap MoveEndLine    = Just . fmap (moveEnd Fwd)
+actionMap MoveBeginLine  = Just . fmap (moveEnd Bwd)
+actionMap KillToEnd      = repeatMaybe $ fmapM (delete Fwd)
 
 specialKeys = M.fromList [ (EvKey KLeft [],   MoveLeft)
                          , (EvKey KRight [],  MoveRight)
@@ -43,7 +57,13 @@ specialKeys = M.fromList [ (EvKey KLeft [],   MoveLeft)
                          , (EvKey KBS [],     DeleteBack)
                          , (EvKey KEnter [],  InsertLine)
                          , (EvKey KEnter [MMeta],  InsertLineTop)
+                         , (EvKey (KASCII 'e') [MCtrl],  MoveEndLine)
+                         , (EvKey (KASCII 'a') [MCtrl],  MoveBeginLine)
+                         , (EvKey (KASCII 'k') [MCtrl],  KillToEnd)
+                         , (EvKey (KASCII 'e') [MMeta, MCtrl],  MoveEnd)
+                         , (EvKey (KASCII 'a') [MMeta, MCtrl],  MoveBegin)
                          ]
+
 
 performAction   :: (Monad m) => Event -> BufferST m Bool
 performAction k = maybe (return False) (\x -> modifyCursor (actionMap x) >> return True) (translateKey k)
